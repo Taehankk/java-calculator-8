485cca0daee694fe8594fc39148733786dfbdf6c
KTH <1514kth@naver.com>
2025-10-16 15:19:34 +0900

    커스텀 구분자 분리 로직 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,38 @@
 package calculator;
 
+import java.io.*;
+import java.util.*;
+
 public class Application {
-    public static void main(String[] args) {
+	static Set<String> separator;
+    public static void main(String[] args) throws Exception {
         // TODO: 프로그램 구현
+    	separator = new HashSet<>();
+    	separator.add(",");
+    	separator.add(":");
+    	
+    	String number_input = splitCustomSeperator();
+    	
+    	System.out.println(separator);
+    	System.out.println(number_input);
+    }
+    
+    public static String splitCustomSeperator() throws Exception {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	String input = br.readLine();
+    	
+    	String[] split_input;
+    	if(input.startsWith("//") && input.contains("\\n")) {
+    		split_input = input.split("\\\\n");
+    		
+    		input = split_input[0].replace("//", "");
+    		
+    		separator.add(input);
+    		
+    		return split_input[1];
+    	}
+    	
+    	return input;
     }
 }
